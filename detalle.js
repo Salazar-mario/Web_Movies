5e8f07ee117c0fbf6d1050e6297bcbab110a1163
agustin salazar <mario.agustin.salazar@gmail.com>
2023-07-21 14:34:27 -0300

    se cambia de api y se agrega detalles de las peliculas

---
+++ detalle.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '5ee161198ef9e0a3c6d9fc3b1ccb8cd3';
+
+    // Obtener el ID de la película de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const movieId = urlParams.get('id');
+
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=credits`)
+        .then(response => response.json())
+        .then(movie => {
+            const movieTitle = document.getElementById('movie-title');
+            const movieReleaseDate = document.getElementById('movie-release-date');
+            const movieOverview = document.getElementById('movie-overview');
+            const movieCast = document.getElementById('movie-cast');
+
+            // Construir la URL de la imagen de la película
+            const posterBaseUrl = 'https://image.tmdb.org/t/p/w500';
+            const posterPath = movie.poster_path;
+            const posterUrl = posterBaseUrl + posterPath;
+
+            // Actualizar elementos HTML con la información de la película
+            movieTitle.textContent = movie.title;
+            movieReleaseDate.textContent = `Año de lanzamiento: ${movie.release_date}`;
+            movieOverview.textContent = `Sinopsis: ${movie.overview}`;
+
+            // Mostrar la imagen de la película
+            const moviePoster = document.createElement('img');
+            moviePoster.src = posterUrl;
+            moviePoster.alt = `${movie.title} Poster`;
+            movieCast.appendChild(moviePoster);
+
+            // Obtener el reparto de la película
+            const cast = movie.credits.cast;
+            let castList = '<p>Reparto: ';
+            for (let i = 0; i < cast.length; i++) {
+                castList += `${cast[i].name} (${cast[i].character})`;
+                if (i < cast.length - 1) {
+                    castList += ', ';
+                }
+            }
+            castList += '</p>';
+            movieCast.innerHTML += castList;
+
+            // Agrega más detalles de la película si es necesario
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+// script.js
 document.addEventListener('DOMContentLoaded', () => {
-    const apiKey = 'd496e6cc'; // Reemplaza con tu clave de API OMDb
+    const apiKey = '5ee161198ef9e0a3c6d9fc3b1ccb8cd3'; // Reemplaza 'TU_CLAVE_DE_API' con tu clave de API de TMDb
 
-    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=`)
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
         .then(response => response.json())
         .then(data => {
-            if (data.Response === 'True') {
-                const movies = data.Search;
-
+            if (data.results && data.results.length > 0) {
+                const movies = data.results;
                 const movieContainer = document.getElementById('movies-list');
+                
                 movies.forEach(movie => {
                     const movieItem = document.createElement('li');
-                    movieItem.textContent = `${movie.Title} (${movie.Year})`;
+
+                    // Crea un enlace (a) con el ID de la película como parámetro en la URL
+                    const movieLink = document.createElement('a');
+                    movieLink.href = `detalle.html?id=${movie.id}`; // Agrega el ID de la película como parámetro en la URL
+                    movieLink.textContent = `${movie.title} (${movie.release_date})`;
+
+                    movieItem.appendChild(movieLink);
                     movieContainer.appendChild(movieItem);
                 });
             } else {
